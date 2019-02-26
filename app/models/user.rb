1ca6e457881a0798780252c2c3e3c6aa2b137bc8
Doug Fitzgerald <dfitzge1@conncoll.edu>
2019-02-26 16:03:20 -0500

    before game position removal

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,11 @@ class User < ApplicationRecord
   has_many :games
   has_secure_password
   validates :username, presence: true, uniqueness: true
+
+  def self.from_token_request request
+    # Returns a valid user, `nil` or raise `Knock.not_found_exception_class_name`
+    # e.g.
+      username = request.params["auth"] && request.params["auth"]["username"]
+      self.find_by username: username
+  end
 end
