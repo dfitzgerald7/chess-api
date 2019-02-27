dd082c2451071859a9ce251d399b7706c3eeeb33
Doug Fitzgerald <dfitzge1@conncoll.edu>
2019-02-26 19:41:25 -0500

    added nested route for games of a user

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   post 'user_token' => 'user_token#create'
   resources :games
-  resources :users
+  resources :users do
+    resources :games, only: [:index]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
